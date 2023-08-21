a84dad43b516f2c79535343bc212d05fbd2d7ab4
Rasmi Jati <jatirasmi65@gmail.com>
2023-08-21 08:28:47 +0545

    created web api using swapper

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        //country controller for every request is different so we use static in list below 
+        static List<Country> countries = new List<Country>();  //blank collection
+
+        //Get api/<CountryController>
+        [HttpGet(Name = "GetCountry")]
+        public IEnumerable<Country> Get()
+        {
+            return countries;
+        }
+
+
+        //Get api/<CountryController>/5
+        [HttpGet("{id}")]
+        public Country Get(int id)
+        {
+            return countries.FirstOrDefault(c=> c.Id == id);
+        }
+
+        //POST api/<CountryController>
+        [HttpPost]
+        public void Post([FromBody] Country country)
+        {
+            countries.Add(country);
+        }
+
+        //PUT api/CountryController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Country country)
+        {
+            int i = countries.FindIndex(c => c.Id == id);
+            if (i >= 0)
+            {
+                countries[i] = country;
+            }
+        }
+
+        //DELETE api/<CountryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            countries.RemoveAll(c => c.Id == id);
+        }
+    }
+}
---
+++ Country.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiDemo
+{
+    public class Country
+    {
+        public int Id { get;set; }
+        public string Name { get;set; }
+        public string Country_Code { get;set; }
+        public string Language { get;set; }
+        public string Capital { get;set; }
+        public string Currency { get;set; }
+        
+    }
+}
